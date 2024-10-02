15e518fec66539290d75e7c858efb4612ecfc773
Jay Son <sweetandsourkiss@gmail.com>
2024-10-02 13:38:50 +0900

    Assignment 27

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tweet
+
 
 # Create your views here.
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+    return render(
+        request,
+        "all_tweets.html",
+        {
+            "tweets": tweets,
+        },
+    )
